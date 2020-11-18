ee639ecd4674c37876d044ea35d3cefb89f0e532
Carlos Andres Chapid Inga <carloschapid@unicauca.edu.co>
2020-11-18 15:16:22 -0500

    first commit

---
+++ src/main/java/co/edu/unicauca/asae/core/base_pdm/BasePdmApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.asae.core.base_pdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasePdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasePdmApplication.class, args);
+	}
+
+}
